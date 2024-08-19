15fb2bb09e148cf702a69d1d95afce27f331ad82
LovatoFT <lovatoft@gmail.com>
2024-08-19 15:18:09 -0300

    cabeçalho ok

--- modelo 2/menu-responsivo/estilos/menu.js
+++ modelo 2/menu-responsivo/estilos/menu.js
@@ -2,10 +2,10 @@ function mostrarMenu() {
     let menuCelular = document.querySelector('.menu-celular');
     if (menuCelular.classList.contains('open')){
         menuCelular.classList.remove('open');
-        document.querySelector('.icon').src="imagens/menu_icon.png";
+        document.querySelector('.icon').src="imagens/menu-close.png";
     
     } else {
         menuCelular.classList.add('open')
-        document.querySelector('.icon').src="imagens/menu-close.png";
+        document.querySelector('.icon').src="imagens/menu-icon.png";
     }
 }   
